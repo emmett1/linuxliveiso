6a8e18d5a15e2cf8fab19ca537104cd9ee057180
emmett1 <emmett1.2miligrams@protonmail.com>
2023-05-08 09:03:34 +0800

    updates

--- liverootfs/root/live_script.sh
+++ liverootfs/root/live_script.sh
@@ -17,22 +17,60 @@ echo "$LIVEUSER:$PASSWORD" | chpasswd -c SHA512
 
 # hostname for live
 case $NAME in
-	Slackware) echo liveiso > /etc/HOSTNAME;;
-	        *) echo liveiso > /etc/hostname;;
+	Slackware) echo linuxliveiso > /etc/HOSTNAME;;
+	        *) echo linuxliveiso > /etc/hostname;;
 esac
 
 # timezone
 case $NAME in
-	Gentoo) echo "$TIMEZONE" > $ROOT/etc/timezone
+	Gentoo) echo UTC > $ROOT/etc/timezone
 	        emerge --config sys-libs/timezone-data;;
 	     *) ln -sf /usr/share/zoneinfo/UTC /etc/localtime;;
 esac
 
-# a little path to avoid these pseudofs mount again, its already handled by initramfs
+# a little patch to avoid these pseudofs mount again, its already handled by initramfs
 if [ "$NAME" = Slackware ]; then	
 	sed -i 's,/sbin/mount -v proc /proc.*,true,' /etc/rc.d/rc.S
 	sed -i 's,/sbin/mount -v sysfs /sys.*,true,' /etc/rc.d/rc.S
 	sed -i 's,/sbin/mount -v -n -t tmpfs tmpfs.*,true,' /etc/rc.d/rc.S
+fi
+
+# voidlinux's specific stuffs
+if [ "$NAME" = Void ]; then
+	[ -d /etc/sv/wpa ] && network=wpa
+	[ -d /etc/sv/NetworkManager ] && network=NetworkManager
+	[ -d /etc/sv/lxdm ] && dm=lxdm
+	[ -d /etc/sv/lightdm ] && dm=lightdm
+	[ -d /etc/sv/sddm ] && dm=sddm
+	[ -d /etc/sv/slim ] && dm=slim
+	services="elogind dbus $dm alsa bluetooth gpm $network"
+	# enable services
+	for d in $services; do
+		[ -d /etc/sv/$d ] && ln -s /etc/sv/$d /etc/runit/runsvdir/default
+	done
+	# use void theme
+	if [ -d /usr/share/slim/themes/slim-void-theme ] && [ -f /etc/slim.conf ]; then
+		sed 's/current_theme.*/current_theme slim-void-theme/' -i /etc/slim.conf
+	fi
+	# use en_US as default locale
+	echo "en_US.UTF-8 UTF-8" >> /etc/default/libc-locales
+	echo "en_US ISO-8859-1" >> /etc/default/libc-locales
+	xbps-reconfigure -f glibc-locales
+fi
+
+if [ "$NAME" = Gentoo ]; then
+	[ -f /etc/init.d/elogind ] && rc-update add elogind boot
+	[ -f /etc/init.d/dbus ] && rc-update add elogind boot
+	[ -f /etc/init.d/display-manager ] && {
+		sed 's,DISPLAYMANAGER=.*,DISPLAYMANAGER=\"slim\",' -i /etc/conf.d/display-manager
+		rc-update add display-manager default
+	}
+fi
+
+# slim autologin
+if [ -f /etc/slim.conf ]; then
+	echo "default_user $LIVEUSER" >> /etc/slim.conf
+	echo "auto_login yes" >> /etc/slim.conf
 fi
 
 # enable sudo permission for all user in live
--- liverootfs/root/post-install.sh
+++ liverootfs/root/post-install.sh
@@ -24,38 +24,6 @@ if [ -f /etc/os-release ]; then
 	. /etc/os-release
 fi
 
-if [ -x $ROOT/etc/rc.d/networkmanager ]; then
-	network=networkmanager
-elif [ -x $ROOT/etc/rc.d/network ]; then
-	network=network
-fi
-
-if [ -x $ROOT/etc/rc.d/lxdm ]; then
-	dm=lxdm
-elif [ -x $ROOT/etc/rc.d/lightdm ]; then
-	dm=lightdm
-elif [ -x $ROOT/etc/rc.d/sddm ]; then
-	dm=sddm
-elif [ -x $ROOT/etc/rc.d/slim ]; then
-	dm=slim
-fi
-
-daemons="sysklogd dbus $dm alsa bluetooth gpm $network"
-
-# enable services
-for d in $daemons; do
-	if [ -x $ROOT/etc/rc.d/$d ]; then
-		if [ "$dd" ]; then
-			dd="$dd $d"
-		else
-			dd="$d"
-		fi
-	fi
-	if [ -d $ROOT/etc/sv/$d ]; then
-		ln -s /etc/sv/$d $ROOT/etc/runit/runsvdir/default
-	fi
-done
-
 # hostname
 case $NAME in
 	Slackware) echo "$HOSTNAME" > $ROOT/etc/HOSTNAME;;
@@ -66,6 +34,7 @@ esac
 case $NAME in
 	Venom*) sed "s;#HARDWARECLOCK=.*;HARDWARECLOCK=\"localtime\";" -i $ROOT/etc/rc.conf
 	        sed "s;#HARDWARECLOCK=.*;HARDWARECLOCK=\"localtime\";" -i $ROOT/etc/runit/runit.conf;;
+	  Void) sed "s,^#HARDWARECLOCK=.*,HARDWARECLOCK=\"localtime\"," -i $ROOT/etc/rc.conf;;
 esac
 
 # timezone
@@ -73,14 +42,15 @@ case $NAME in
 	Slackware) ln -sf /usr/share/zoneinfo/$TIMEZONE $ROOT/etc/localtime;;
 	   Gentoo) echo "$TIMEZONE" > $ROOT/etc/timezone
 	           emerge --config sys-libs/timezone-data;;
+	     Void) sed "s,^#TIMEZONE=.*,TIMEZONE=\"$TIMEZONE\"," -i $ROOT/etc/rc.conf;;
 	   Venom*) sed "s;#TIMEZONE=.*;TIMEZONE=\"$TIMEZONE\";" -i $ROOT/etc/rc.conf
                sed "s;#TIMEZONE=.*;TIMEZONE=\"$TIMEZONE\";" -i $ROOT/etc/runit/runit.conf;;
 esac
 
 # keymap
 case $NAME in
-	Slackware) sed -i "s,us.map,$KEYMAP.map," $ROOT/etc/rc.d/rc.keymap;;
 	   Gentoo) echo "keymap=\"$KEYMAP\"" > $ROOT/etc/conf.d/keymaps;;
+	     Void) sed "s,^#KEYMAP=.*,KEYMAP=\"$KEYMAP\"," -i $ROOT/etc/rc.conf;;
    	   Venom*) sed "s;#KEYMAP=.*;KEYMAP=\"$KEYMAP\";" -i $ROOT/etc/rc.conf
                sed "s;#KEYMAP=.*;KEYMAP=\"$KEYMAP\";" -i $ROOT/etc/runit/runit.conf;;
 esac
@@ -91,22 +61,73 @@ case $NAME in
 	   Gentoo) sed "s/#$LOCALE/$LOCALE/g" -i $ROOT/etc/locale.gen
 	           echo "LANG=$LOCALE.UTF-8" > $ROOT/etc/env.d/02locale
 	           _run locale-gen;;
+	     Void) sed "s/#$LOCALE/$LOCALE/" -i $ROOT/etc/default/libc-locales
+	           echo "LANG=$LOCALE.UTF-8" > $ROOT/etc/locale.conf
+	           _run xbps-reconfigure -f glibc-locales;;
 	   Venom*) sed "s/#$LOCALE/$LOCALE/" -i $ROOT/etc/locales
 	           echo "LANG=$LOCALE.UTF-8" > $ROOT/etc/locale.conf
 	           _run genlocales;;
 esac
 
-# daemons
-case $NAME in
-	Venom*) sed "s;#DAEMONS=.*;DAEMONS=\"$dd\";" -i $ROOT/etc/rc.conf;;
-esac
-
+# fstab
 case $NAME in
 	Slackware) echo "/dev/fd0 /mnt/floppy auto noauto,owner 0 0" >> $ROOT/etc/fstab
 	           echo "devpts /dev/pts devpts gid=5,mode=620 0 0" >> $ROOT/etc/fstab
 	           echo "proc /proc proc defaults 0 0" >> $ROOT/etc/fstab
 	           echo "tmpfs /dev/shm tmpfs nosuid,nodev,noexec 0 0" >> $ROOT/etc/fstab;;
+	     Void) echo "tmpfs /tmp tmpfs defaults,nosuid,nodev 0 0" >> $ROOT/etc/fstab;;
 esac
+
+if [ "$NAME" = Void ]; then
+	[ -d $ROOT/etc/sv/wpa ] && network=wpa
+	[ -d $ROOT/etc/sv/NetworkManager ] && network=NetworkManager
+	[ -d $ROOT/etc/sv/lxdm ] && dm=lxdm
+	[ -d $ROOT/etc/sv/lightdm ] && dm=lightdm
+	[ -d $ROOT/etc/sv/sddm ] && dm=sddm
+	[ -d $ROOT/etc/sv/slim ] && dm=slim
+	services="elogind dbus $dm alsa bluetooth gpm $network"
+	# enable services
+	for d in $services; do
+		[ -d $ROOT/etc/sv/$d ] && ln -s /etc/sv/$d $ROOT/etc/runit/runsvdir/default
+	done
+	# use void theme	
+	if [ -d $ROOT/usr/share/slim/themes/slim-void-theme ] && [ -f $ROOT/etc/slim.conf ]; then
+		sed 's/current_theme.*/current_theme slim-void-theme/' -i $ROOT/etc/slim.conf
+	fi
+fi
+
+if [ "$NAME" = "Venom Linux" ]; then
+	[ -x $ROOT/etc/rc.d/network ] && network=network
+	[ -x $ROOT/etc/rc.d/networkmanager ] && network=networkmanager
+	[ -x $ROOT/etc/rc.d/lxdm ] && dm=lxdm
+	[ -x $ROOT/etc/rc.d/lightdm ] && dm=lightdm
+	[ -x $ROOT/etc/rc.d/sddm ] && dm=sddm
+	[ -x $ROOT/etc/rc.d/slim ] && dm=slim
+	daemons="sysklogd dbus $dm alsa bluetooth gpm $network"
+	# enable services
+	for d in $daemons; do
+		if [ -x $ROOT/etc/rc.d/$d ]; then
+			if [ "$dd" ]; then
+				dd="$dd $d"
+			else
+				dd="$d"
+			fi
+		fi
+		if [ -d $ROOT/etc/sv/$d ]; then
+			ln -s /etc/sv/$d $ROOT/etc/runit/runsvdir/default
+		fi
+	done
+	sed "s;#DAEMONS=.*;DAEMONS=\"$dd\";" -i $ROOT/etc/rc.conf
+fi
+
+if [ "$NAME" = Gentoo ]; then
+	[ -f $ROOT/etc/init.d/elogind ] && _run rc-update add elogind boot
+	[ -f $ROOT/etc/init.d/dbus ] && _run rc-update add elogind boot
+	[ -f $ROOT/etc/init.d/display-manager ] && {
+		sed 's,DISPLAYMANAGER=.*,DISPLAYMANAGER=\"slim\"' -i $ROOT/etc/conf.d/display-manager
+		_run rc-update add display-manager default
+	}
+fi
 
 # create user
 # 'useradd -R' not copy all skel files, use _run instead
@@ -119,8 +140,12 @@ echo "root:$ROOT_PSWD" | chpasswd -R $ROOT -c SHA512
 
 # initramfs
 case $NAME in
-	Venom*) _run mkinitramfs;;
-	Gentoo) _run emerge --config sys-kernel/gentoo-kernel
+	Slackware) echo "#!/bin/sh" > $ROOT/tmp/geninitrd.sh
+	           _run /usr/share/mkinitrd/mkinitrd_command_generator.sh | grep ^mkinitrd >> /tmp/geninitrd.sh
+	           _run sh /tmp/geninitrd.sh
+	           rm -f $ROOT/tmp/geninitrd.sh;;
+	   Venom*) _run mkinitramfs;;
+	   Gentoo) _run emerge --config sys-kernel/gentoo-kernel
 esac
 
 # grub
